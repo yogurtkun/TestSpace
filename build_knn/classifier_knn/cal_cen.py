9c63546977093471714f22976cd83a852c8f3d8f
yogurtkun <yogurtkun@126.com>
2016-05-21 14:35:31 +0800

    test of center vector

--- build_knn/classifier_knn/cal_cen.py
+++ build_knn/classifier_knn/cal_cen.py
@@ -3,6 +3,10 @@ import re
 from function_tool import load_sparse_csr,save_sparse_csr
 from sklearn import preprocessing
 
+'''
+该部分将所有涉及到的向量归一化,并且存储
+'''
+
 tran_mat = load_sparse_csr('../itera_knn/traning_vector')
 norm_tran_mat = preprocessing.normalize(tran_mat)
 
@@ -17,4 +21,3 @@ for parent,dirnames,filenames in os.walk(data_dir):
         file_vector = load_sparse_csr(data_dir+file_id)
         norm_file_vector = preprocessing.normalize(file_vector)
         save_sparse_csr('./file_vector/'+file_id,norm_file_vector)
-        break
---
+++ build_knn/classifier_knn/find_cen.py
@@ -0,0 +1,34 @@
+import numpy as np
+from function_tool import load_sparse_csr,load_list
+import json
+from math import pi
+from scipy.sparse import vstack
+
+'''
+计算各类向量的中心
+'''
+
+tran_npz = load_sparse_csr('./tran_matrix')
+with open('../info_list.trn','r',encoding='utf-8') as file:
+    info_list = json.load(file)
+
+class_id_list = list(map(lambda x:x[1],info_list)) #得到文章分类结果的list
+
+cen_vector_dict = {}
+
+for i in range(len(class_id_list)):
+    file_class = class_id_list[i]
+    if not file_class in cen_vector_dict:
+        cen_vector_dict[file_class] = tran_npz[i]
+    else:
+        cen_vector_dict[file_class] += tran_npz[i]
+
+temp_list = []
+for id,vec in cen_vector_dict.items():
+    temp_list.append((id,vec))
+
+temp_list.sort(key=lambda x:x[0])
+
+cen_mat = list(map(lambda x:x[1],temp_list))
+res_mat = vstack(cen_mat)
+
